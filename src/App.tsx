2376f2dac1a0b4cb0620810b1d2c7e3e13a525bf
smart-dev318 <62882534+smart-dev318@users.noreply.github.com>
2022-07-04 15:09:58 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { CardBody } from "./component/cardBody";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CardBody />
+    </div>
+  );
+}
---
+++ src/component/cardBody.tsx
@@ -0,0 +1,93 @@
+import eventCard from "./eventCard";
+import {
+  Color,
+  Rect,
+  Stroke,
+  ContainerComponent,
+  TextComponent,
+  RectangleComponent,
+  EllipseComponent
+} from "../componentTypes";
+
+const getRect = (rect: Rect | undefined) => {
+  return rect
+    ? {
+        position: `absolute`,
+        left: `${rect.x}px`,
+        top: `${rect.y}px`,
+        width: `${rect.width}px`,
+        height: `${rect.height}px`
+      }
+    : "";
+};
+
+const getStroke = (stroke: Stroke | undefined) => {
+  return stroke
+    ? {
+        border: `${stroke.width}px solid rgb(${stroke.color.r}, ${stroke.color.g}, ${stroke.color.b}, ${stroke.color.a})`
+      }
+    : {};
+};
+
+const rgbaColor = (color: Color | undefined) => {
+  return color
+    ? `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${
+        color.a * 255
+      })`
+    : "";
+};
+
+const Container = ({ component }: { component: ContainerComponent }) => {
+  console.log("!", component.cornerRadius);
+  let containerStyle = {
+    ...getRect(component.frame),
+    backgroundColor: rgbaColor(component.backgroundColor),
+    ...getStroke(component.border),
+    borderRadius: `${component.cornerRadius}px`
+  };
+
+  return (
+    <div style={containerStyle}>
+      {component.children.map((item) => {
+        switch (item.type) {
+          case "container":
+            return <Container component={item} />;
+          case "text":
+            return <Text component={item} />;
+          case "rectangle":
+            return <Rectangle component={item} />;
+          case "ellipse":
+            return <Ellipse component={item} />;
+        }
+      })}
+    </div>
+  );
+};
+
+const Text = ({ component }: { component: TextComponent }) => {
+  let textStyle = {
+    ...getRect(component.frame),
+    fontSize: `${component.fontSize}px`,
+    fontWeight: `${component.fontWeight}`,
+    fontFamily: `${component.fontFamily}`,
+    color: rgbaColor(component.textColor)
+  };
+  return <div style={textStyle}>{component.content}</div>;
+};
+
+const Rectangle = ({ component }: { component: RectangleComponent }) => {
+  let rectangleStyle = {
+    ...getRect(component.frame),
+    backgroundColor: rgbaColor(component.backgroundColor),
+    borderRadius: `${component.cornerRadius}px`
+  };
+  return <div style={rectangleStyle} />;
+};
+
+const Ellipse = ({ component }: { component: EllipseComponent }) => {
+  return <>Ellipse</>;
+};
+
+export const CardBody = () => {
+  return <Container component={eventCard} />;
+};
